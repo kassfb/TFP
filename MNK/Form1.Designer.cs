f7cac881e5da9d38a73252e0a1aa8c85419f76e8
Aleksei Kirsanov <aleksey07091998@mail.ru>
2022-04-17 14:09:43 +0300

    init

---
+++ MNK/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace MNK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._dgw_xy = new System.Windows.Forms.DataGridView();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._bt_open = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this._bt_culc = new System.Windows.Forms.Button();
+            this._picb_graphic = new System.Windows.Forms.PictureBox();
+            this._bt_drawG = new System.Windows.Forms.Button();
+            this._tb_eps = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this._dgw_xy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._picb_graphic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _dgw_xy
+            // 
+            this._dgw_xy.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this._dgw_xy.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this._dgw_xy.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X,
+            this.Y});
+            this._dgw_xy.Location = new System.Drawing.Point(3, 59);
+            this._dgw_xy.Name = "_dgw_xy";
+            this._dgw_xy.RowHeadersVisible = false;
+            this._dgw_xy.RowHeadersWidth = 80;
+            this._dgw_xy.Size = new System.Drawing.Size(281, 379);
+            this._dgw_xy.TabIndex = 0;
+            // 
+            // X
+            // 
+            this.X.HeaderText = "X";
+            this.X.Name = "X";
+            // 
+            // Y
+            // 
+            this.Y.HeaderText = "Y";
+            this.Y.Name = "Y";
+            // 
+            // _bt_open
+            // 
+            this._bt_open.Location = new System.Drawing.Point(2, 3);
+            this._bt_open.Name = "_bt_open";
+            this._bt_open.Size = new System.Drawing.Size(90, 25);
+            this._bt_open.TabIndex = 1;
+            this._bt_open.Text = "Открыть файл";
+            this._bt_open.UseVisualStyleBackColor = true;
+            this._bt_open.Click += new System.EventHandler(this._bt_open_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(290, 382);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(666, 51);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
+            // 
+            // _bt_culc
+            // 
+            this._bt_culc.Location = new System.Drawing.Point(98, 3);
+            this._bt_culc.Name = "_bt_culc";
+            this._bt_culc.Size = new System.Drawing.Size(90, 25);
+            this._bt_culc.TabIndex = 4;
+            this._bt_culc.Text = "Расчет";
+            this._bt_culc.UseVisualStyleBackColor = true;
+            this._bt_culc.Click += new System.EventHandler(this._bt_culc_Click);
+            // 
+            // _picb_graphic
+            // 
+            this._picb_graphic.Location = new System.Drawing.Point(290, 3);
+            this._picb_graphic.Name = "_picb_graphic";
+            this._picb_graphic.Size = new System.Drawing.Size(675, 435);
+            this._picb_graphic.TabIndex = 5;
+            this._picb_graphic.TabStop = false;
+            // 
+            // _bt_drawG
+            // 
+            this._bt_drawG.Enabled = false;
+            this._bt_drawG.Location = new System.Drawing.Point(194, 3);
+            this._bt_drawG.Name = "_bt_drawG";
+            this._bt_drawG.Size = new System.Drawing.Size(90, 25);
+            this._bt_drawG.TabIndex = 6;
+            this._bt_drawG.Text = "График";
+            this._bt_drawG.UseVisualStyleBackColor = true;
+            this._bt_drawG.Click += new System.EventHandler(this._bt_drawG_Click);
+            // 
+            // _tb_eps
+            // 
+            this._tb_eps.Location = new System.Drawing.Point(194, 33);
+            this._tb_eps.Name = "_tb_eps";
+            this._tb_eps.Size = new System.Drawing.Size(90, 20);
+            this._tb_eps.TabIndex = 7;
+            this._tb_eps.Text = "0,000007";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(2, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Параметр фильтрации eps =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 445);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._tb_eps);
+            this.Controls.Add(this._bt_drawG);
+            this.Controls.Add(this._picb_graphic);
+            this.Controls.Add(this._bt_culc);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this._bt_open);
+            this.Controls.Add(this._dgw_xy);
+            this.Name = "Form1";
+            this.Text = "Поиск и фильтрация точек фазового перехода";
+            ((System.ComponentModel.ISupportInitialize)(this._dgw_xy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._picb_graphic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView _dgw_xy;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.Button _bt_open;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button _bt_culc;
+        private System.Windows.Forms.PictureBox _picb_graphic;
+        private System.Windows.Forms.Button _bt_drawG;
+        private System.Windows.Forms.TextBox _tb_eps;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MNK/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace MNK
+{
+    public partial class Form1 : Form
+    {
+        float Xmin, Xmax, Ymin, Ymax;
+        List<float> draw_points = new List<float>(4);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void _bt_open_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            StreamReader str = new StreamReader(filename);
+            string[] lines = File.ReadAllLines(filename);
+            string buffer;
+            double[,] num = new double[lines.Length / 2, 2];
+            for (int x = 0; x < num.GetLength(0); x++)
+            {
+                for (int y = 0; y < num.GetLength(1); y++)
+                {
+                    buffer = str.ReadLine();
+                    num[x, y] = Convert.ToDouble(buffer);
+                }
+            }
+
+            _dgw_xy.RowCount = num.GetLength(0);
+            _dgw_xy.ColumnCount = num.GetLength(1);
+            for (int i = 0; i < num.GetLength(0); i++)
+                for (int j = 0; j < num.GetLength(1); j++)
+                    _dgw_xy.Rows[i].Cells[j].Value = num[i, j];
+
+            //// проверяем выводом в RTB
+            //for (int i = 0; i < num.GetLength(0); i++)
+            //    for (int j = 0; j < num.GetLength(1); j++)
+            //        richTextBox1.Text+="num["+i+"]["+j+"]="+ num[i, j].ToString() + "\n";
+        }
+        private void _bt_culc_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter("num_test.txt", false, System.Text.Encoding.Default))
+            {
+                double[,] num = new double[_dgw_xy.RowCount, 2];
+                for (int i = 0; i < _dgw_xy.RowCount; i++)
+                {
+                    for (int j = 0; j < _dgw_xy.ColumnCount; j++)
+                    {
+                            num[i, j] = Convert.ToDouble(_dgw_xy.Rows[i].Cells[j].Value);
+                            sw.WriteLine(num[i, j]);
+                    }
+                }
+                culc(num);
+            }
+        }
+
+        public void culc(double[,] num)
+        {
+            using (StreamWriter sw = new StreamWriter("result_TFP.txt", false, System.Text.Encoding.Default))
+            {
+                double[] x = new double[num.GetLength(0)];
+                double[] y = new double[num.GetLength(0)];
+                double[] y_teoretic = new double[num.GetLength(0)];
+                //PointF[] draw_points = new PointF[num.GetLength(0)];
+                for (int i = 0; i < num.GetLength(0); i++)
+                {
+                    x[i] = num[i, 0];
+                    y[i] = num[i, 1];
+                }
+                Xmin = (float)x.Min();
+                Xmax = (float)x.Max();
+                Ymin = (float)y.Min();
+                Ymax = (float)y.Max();
+                double m_diff = 0;
+                double eps = Convert.ToDouble(_tb_eps.Text);
+                int phase_transition_count = 0;
+                draw_points.Clear();
+                //draw_points.Add((float)x[0]);
+                //draw_points.Add((float)y[0]);
+                System.Diagnostics.Stopwatch my_timer = new Stopwatch();
+                my_timer.Start();
+                for (int i = 0; i < num.GetLength(0) - 2; i++)
+                {
+                    y_teoretic[i + 2] = (x[i + 2] * (y[i + 1] - y[i]) + x[i + 1] * y[i] - x[i] * y[i + 1]) / (x[i + 1] - x[i]);
+                    m_diff = Math.Abs(y_teoretic[i + 2] - y[i + 2]);
+                    if (m_diff > eps)
+                    {
+                        phase_transition_count++;
+                        sw.WriteLine(/*" m_diff= " + m_diff + " phase_transition*/" X= " + x[i + 1] + " Y= " + y[i + 1]);
+                        draw_points.Add((float)x[i + 1]);
+                        draw_points.Add((float)y[i + 1]/*y_teoretic[i + 1]*/);
+                    }
+                }
+                my_timer.Stop();
+                sw.WriteLine("TIME = " + my_timer.Elapsed.TotalMilliseconds / 1000.0 + " sec");
+                //sw.WriteLine("phase_transition_count= " + phase_transition_count);
+                //sw.WriteLine("END");
+                draw_points.Add((float)x.Last());
+                draw_points.Add((float)/*y_teoretic*/y.Last());
+            }
+            _bt_drawG.Enabled = true;
+        }
+
+        private void _bt_drawG_Click(object sender, EventArgs e)
+        {
+            DrawGraphic();
+        }
+
+        float Xe(float X)
+        {
+            return (float)_picb_graphic.Size.Width * (X - Convert.ToSingle(Xmin)) / (Convert.ToSingle(Xmax) - Convert.ToSingle(Xmin));
+        }
+
+        float Ye(float Y)
+        {
+            return (float)_picb_graphic.Size.Height * (1 - (Y - Convert.ToSingle(Ymin)) / (Convert.ToSingle(Ymax) - Convert.ToSingle(Ymin)));
+        }
+
+        public void DrawGraphic()
+        {
+            Graphics g = _picb_graphic.CreateGraphics();
+
+            Pen my_pen = new Pen(Color.Black, 2);
+            Pen red_pen = new Pen(Color.Red, 1);
+            Pen blue_pen = new Pen(Color.Blue, 1);
+            //размеры PictureBox
+            int pb_width = _picb_graphic.Width;
+            int pb_height = _picb_graphic.Height;
+            g.Clear(Color.White);
+
+            double[,] num = new double[_dgw_xy.RowCount, 2];
+            for (int i = 0; i < _dgw_xy.RowCount; i++)
+            {
+                for (int j = 0; j < _dgw_xy.ColumnCount; j++)
+                {
+                        num[i, j] = Convert.ToDouble(_dgw_xy.Rows[i].Cells[j].Value);
+                }
+            }
+            //Экспериментальные точки (черные)
+            PointF[] all_points = new PointF[num.GetLength(0)];
+            for (int i = 0; i < num.GetLength(0); i++)
+            {
+                all_points[i].X = (float)num[i, 0];
+                all_points[i].Y = (float)num[i, 1];
+                g.FillRectangle(new SolidBrush(Color.Black), new RectangleF(Xe(all_points[i].X), Ye(all_points[i].Y), 2, 2));
+                //g.FillRectangle(new SolidBrush(Color.Black), all_points[i].X, all_points[i].Y, 2, 2);
+                //_bm_grap.SetPixel(Convert.ToInt32(all_points[i].X),Convert.ToInt32(all_points[i].Y), Color.Black);
+            }
+
+            //отрисовка линий по экспериментальным точкам
+            for (int i = 0; i < num.GetLength(0) - 1; i++)
+            {
+                g.DrawLine(my_pen, Xe(all_points[i].X), Ye(all_points[i].Y), Xe(all_points[i + 1].X), Ye(all_points[i + 1].Y));
+            }
+
+            //Теоретические точки(красные)
+            //for (int i = 0; i < draw_points.Count - 1; i += 2)
+            //{
+            //    g.FillRectangle(new SolidBrush(Color.Red), new RectangleF(Xe(draw_points[i]), Ye(draw_points[i + 1]), 4, 4));
+            //}
+
+            //Красне вертикальныые линии
+            for (int i = 0; i < draw_points.Count - 1; i += 2)
+            {
+                g.DrawLine(red_pen, Xe(draw_points[i]), Ye(draw_points[i + 1]), Xe(draw_points[i]), Ye(Ymax));
+                g.DrawLine(red_pen, Xe(draw_points[i]), Ye(draw_points[i + 1]), Xe(draw_points[i]), Ye(Ymin));
+            }
+
+            //отрисовка линий по текущим Y в коридоре < eps
+            //for (int i = 0; i <= draw_points.Count - 4; i += 2)
+            //{
+            //    g.DrawLine(red_pen, Xe(draw_points[i]), Ye(draw_points[i + 1]), Xe(draw_points[i + 2]), Ye(draw_points[i + 3]));
+            //}
+
+            //Начало координат у PictureBox слева сверху [ (0,0 - левый верхний угол) (width,height - правый нижний) (width,0 - правый верхний) (0, height - левый нижний)]
+        }
+    }
+}
